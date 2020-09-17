9a9c7dd46a4244eead72ff5077e8e320679aebd3
venetta6666 <vendetta6666@hotmail.com>
2020-09-16 20:26:02 -0400

    Complete Assignment

--- exercises/01-intro-variables-numbers/intro-variables-numbers.js
+++ exercises/01-intro-variables-numbers/intro-variables-numbers.js
@@ -1,38 +1,45 @@
 function myName() {
   // Create a variable called myName
+  let myName = "Mike"
   return myName;
 }
+console.log(myName("Mike"))
 
 function setTeachersNames(teachersName) {
   // change code below this line so that we can set the variable.
-  const teachers = null;
+  let teachers = null;
   teachers = teachersName; //  <- this is invalid because teachers is a const. How can we fix this?
 
   return teachers;
 }
+console.log("Chris")
 
 /*** return the sum of both arguments  ***/
 function add(a, b) {
-  let sum;
+  let sum = a + b;
 
   return sum;
 }
+console.log(add(5,6))
 
 /*** Using the increment operator, add a year to the argument and make me older */
 function ageMeByAYear(age) {
-  return age;
+  return age+=1;
 }
+console.log(ageMeByAYear(33))
 
 /*** Using the decrement operator make me age decrement by a year  ***/
 function makeMeYoungerByAYear(age) {
-  return age;
+  return age-=1;
 }
+console.log(makeMeYoungerByAYear(33))
 
 /** Take the argument that will be a string and return an integer  */
 function convertStringToInt(str) {
-  let int;
+  let int = Number(str);
   return int;
 }
+console.log(convertStringToInt("6"))
 
 /*** Using the String method indexOf find the first position of the letter in the name  */
 /***
@@ -44,17 +51,19 @@ function convertStringToInt(str) {
  *
  */
 function getTheCharacterPosition(name, letter) {
-  let characterPosition;
+  let characterPosition = name.indexOf(letter);
   return characterPosition;
 }
+console.log(getTheCharacterPosition("Mike","e"))
 
 /** Take the argument and return the length of the argument */
 
 function stringLength(str) {
-  let strLength;
+  let strLength = str.length;
 
   return strLength;
 }
+console.log(stringLength("String"))
 
 /***  return the last character of the argument
  *  ie Washington
@@ -64,10 +73,10 @@ function stringLength(str) {
  */
 
 function getLastCharacter(str) {
-  let lastCharacter;
-
+  let lastCharacter = str.charAt(str.length-1);
   return lastCharacter;
 }
+console.log(getLastCharacter("Awesome"))
 
 /** Return the last part of a place name
  *
@@ -82,10 +91,11 @@ function getLastCharacter(str) {
  */
 
 function getLastWordInPlaceName(place) {
-  let newPlace;
+  let newPlace = place.slice(place.indexOf(' ') - place.length);
 
   return newPlace;
 }
+console.log(getLastWordInPlaceName("San Diego"))
 
 /***
  *  Using Conditonal logic compare both arguments and return the largest argument
@@ -93,10 +103,10 @@ function getLastWordInPlaceName(place) {
  */
 
 function getLargerNumber(arg1, arg2) {
-  let largestNumber;
-
+  let largestNumber = arg1 > arg2 ? arg1 : arg2;
   return largestNumber;
 }
+console.log(getLargerNumber(5,7))
 
 /*** A person just got married and they need their last name replaced.
  * The function accepts two arguments first agrument being the fullname of the newlywed.
@@ -112,9 +122,11 @@ function getLargerNumber(arg1, arg2) {
 
 function replaceLastName(fullName, newLastName) {
   let newFullName;
+  let firstName = fullName.substring(0,fullName.indexOf(' '));
+  newFullName = firstName + ' ' + newLastName;
   return newFullName;
 }
-
+console.log(replaceLastName("Mike Jones","Cobb"))
 /***
  *  Capitalize the first letter in a last name
  *  i.e John smith -> John Smith
@@ -122,9 +134,14 @@ function replaceLastName(fullName, newLastName) {
 
 function capitalizeLastName(fullName) {
   let capitalizeLastName;
-
+  firstLetterLastName = fullName.charAt(fullName.indexOf(' ')+1);
+  firstName = fullName.substring(0, fullName.indexOf(' '));
+  lastName = fullName.slice(fullName.indexOf(' ') - fullName.length)
+  lastName = lastName.replace(firstLetterLastName,firstLetterLastName.toUpperCase())
+  capitalizeLastName = firstName + lastName
   return capitalizeLastName;
 }
+console.log(capitalizeLastName("Michael jones"))
 
 /**
  * Ignore me. This is for the tests
