a5a00aa521b84ae601e6b5ce12c6118835e2fee5
Matina <matinaspatsos@gmail.com>
2020-11-15 12:00:06 -0500

    Axios, async & await and module pattern solutions

--- test/24-module-pattern/module-pattern.spec.js
+++ test/24-module-pattern/module-pattern.spec.js
@@ -6,19 +6,19 @@ const {
   TeacherGradeBook,
 } = require("../../exercises/24-module-pattern/module-pattern.js");
 
-describe("exercises/24-module-pattern/module-pattern.js", () => {
-  describe("TeacherGradeBook.getPassingStudents()", () => {
-    it("should return a list of passing students", () => {
-      const passingStudents = TeacherGradeBook.getPassingStudents();
-      const students = passingStudents.map((student) => student.firstName);
-      expect(students).to.be.equalTo(["John", "Bill", "Joe", "Billy"]);
-    });
-  });
+describe("xexercises/24-module-pattern/module-pattern.js", () => {
   describe("TeacherGradeBook.getFailingStudents()", () => {
     it("should return a failing list of students", () => {
       const failingStudents = TeacherGradeBook.getFailingStudents();
       const students = failingStudents.map((student) => student.firstName);
       expect(students).to.be.equalTo(["Jane", "Michael"]);
+    });
+  });
+  describe("TeacherGradeBook.getPassingStudents()", () => {
+    it("should return a list of passing students", () => {
+      const passingStudents = TeacherGradeBook.getPassingStudents();
+      const students = passingStudents.map((student) => student.firstName);
+      expect(students).to.be.equalTo(["John", "Bill", "Joe", "Billy"]);
     });
   });
 });
